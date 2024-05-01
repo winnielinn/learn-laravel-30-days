4287fc0e55d16d2bce01c49780fff300094e5409
winnielinn <yaysa903@gmail.com>
2024-05-01 16:04:31 +0800

    View data and route wildcards

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,63 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'PHP Developer',
+                'salary' => '$30,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Python Developer',
+                'salary' => '$40,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Java Developer',
+                'salary' => '$50,000'
+            ]
+        ]
+    ]);
+});
+
+Route::get('/job/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'PHP Developer',
+            'salary' => '$30,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Python Developer',
+            'salary' => '$40,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Java Developer',
+            'salary' => '$50,000'
+        ]
+    ];
+
+    // Closure
+    // $job = Arr::first($jobs, function ($job) use ($id) {
+    //     return $job['id'] == $id;
+    // });
+
+    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
+
+    return view('job', [
+        'job' => $job
+    ]);
 });
 
 Route::get('/contact', function () {
